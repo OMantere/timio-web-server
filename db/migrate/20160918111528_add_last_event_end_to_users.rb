7c28d2b6cb12db5f90e47627598d21254a0864e1
Oskari Mantere <osku.mantere@gmail.com>
2016-09-26 13:59:02 +0300

    REmove sqlite

--- db/migrate/20160918111528_add_last_event_end_to_users.rb
+++ db/migrate/20160918111528_add_last_event_end_to_users.rb
@@ -1,5 +1,6 @@
 class AddLastEventEndToUsers < ActiveRecord::Migration[5.0]
   def change
+    enable_extension "hstore"
     add_column :users, :last_event_end, :hstore, default: '', null: false
   end
 end
